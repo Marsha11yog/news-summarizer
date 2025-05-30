0fffe2ab6965afe7c1d9606481ae0c6207be6ba5
Marsha11yog <vsyogesh01@gmail.com>
2025-05-30 17:46:12 +0800

    Update app.py
    The importing of streamlit failed so I have updated the code , from my vs.

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 print ("Hello World!")
+import streamlit as st
+from scrapper import extract_article
+from summarise import summarize_text
+
+st.title("News Article Summarizer")
+
+url = st.text_input("Paste the article URL below:")
+
+if st.button("Summarize"):
+    title, text = extract_article(url)
+    summary = summarize_text(text)
+    st.subheader(f"Title: {title}")
+    st.markdown(summary)
 
 
